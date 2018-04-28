ac5698d193e415ae400cbf471fe282a67b43c2ea
Maciej Aleksandrowicz <maciejaleksandrowicz@almatron.pl>
2018-04-28 23:14:16 +0200

    fixed anim

--- game.js
+++ game.js
@@ -1,7 +1,10 @@
 Modules.Game = Game
 
 function Game() {
+    var self = this
     this.lastId = 1
+    this.frame = 0
+    this.heartBeat = setInterval(10,function() { self.nextFrame() })
 }
 
 Game.prototype.startWithBoard = function(board) {
@@ -13,6 +16,28 @@ Game.prototype.startWithBoard = function(board) {
     
     entity.putTo(0,0)
 
+}
+
+Game.prototype.moveSelected = function(x,y) {
+    if (this.selected) {
+        var selected = this.selected
+        var startAt = {x: selected.x, y: selected.y}
+
+        var board = this.boardApi
+        board.animMove(100, selected, x,y, function(success) {
+            if (success) {
+                var fields = board.fields
+                fields.at(startAt.x, startAt.y).put(null)
+                fields.at(startAt.x+x, startAt.y+y).put(selected)
+                selected.x += x
+                selected.y += y
+
+            }
+            else {
+                throw "not implemented"
+            }
+        })
+    }
 }
 
 Game.prototype.onClick = function(x,y) {
